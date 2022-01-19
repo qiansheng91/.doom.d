cbaaea8b807955143bce1cdd8afd64d4bf46aa0c
千乘 <gavin.zx@alibaba-inc.com>
2022-01-19 13:08:41 +0800

    Init project

--- init.el
+++ init.el
@@ -21,9 +21,9 @@
 
        :completion
        company           ; the ultimate code completion backend
-       (helm +fuzzy)             ; the *other* search engine for love and life
+       ;;(helm +fuzzy)             ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
-       ;;(ivy +fuzzy +prescient)               ; a search engine for love and life
+       (ivy +fuzzy +prescient)               ; a search engine for love and life
        vertico           ; the search engine of the future
 
        :ui

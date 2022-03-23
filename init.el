3d2339c89da8c39a1b7a77b80322a3b991da7011
千乘 <gavin.zx@alibaba-inc.com>
2022-03-23 13:56:32 +0800

    add package and change theme

--- init.el
+++ init.el
@@ -151,7 +151,7 @@
        ;;nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
        (org +dragndrop)               ; organize your plain life in plain text
-       ;;php               ; perl's insecure younger brother
+       (php +lsp)               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
        (python +pyright +lsp +pyenv +poerty)            ; beautiful is better than ugly
